4ca50c760f69fb0ff3d983c12c0de7580d610e08
Dilip Chaudhary <dilipkumarc@hyperlinkinfosystem.net.in>
2025-11-24 13:57:00 +0530

    changes

--- resources/views/website/project-progress.blade.php
+++ resources/views/website/project-progress.blade.php
@@ -248,7 +248,7 @@
                                             {{ __('messages.timeline') }}
                                         </button>
                                         <button
-                                            onclick="window.location.href='/website/project/'+currentProjectId+'/daily-logs'"
+                                            onclick="window.location.href='/website/project/'+currentProjectId+'/daily-log-role-descriptions'"
                                             class="btn btn-outline-primary d-flex align-items-center gap-2 rounded-5 svg-hover-white">
                                             <svg width="15" height="16" viewBox="0 0 15 16" fill="none"
                                                 xmlns="http://www.w3.org/2000/svg">
